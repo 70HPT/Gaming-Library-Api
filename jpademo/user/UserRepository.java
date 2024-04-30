81f130e191e9975ec0f60c904524fffe9527f005
Chris R. Nieves <142316550+ChrisNieves23@users.noreply.github.com>
2024-04-30 04:02:00 -0400

    Added files that were chnaged
    These are updated versions for Admin use-cases and templates cotain new html files

---
+++ jpademo/user/UserRepository.java
@@ -0,0 +1,23 @@
+
+package com.csc340.jpademo.user;
+
+/**
+ *
+ * @author chrisnieves
+ */
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+   
+   
+    User findByUsername(String username);
+}
+
+
+
+
+
